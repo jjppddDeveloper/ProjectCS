a6daae487138ca055da0c3ac22b5656e4f136f55
jjppddDeveloper <jjppdd.userrr@gmail.com>
2023-09-23 01:09:03 +0900

    超簡易的なForm画面

--- MyApp.cs
+++ MyApp.cs
@@ -1,7 +1,20 @@
 using System;
+using System.Drawing;
+using System.Windows.Forms;
 
-public class Hello{
-    public static void Main(){
-        Console.WriteLine("Hello World!!");
+public class Hello
+{
+    public static void Main()
+    {
+        Application.Run(new Form1());
+    }
+}
+
+class Form1 : Form
+{
+    public Form1()
+    {
+        this.Text = "Hello World !!";
+        this.BackColor = SystemColors.Highlight;
     }
 }
