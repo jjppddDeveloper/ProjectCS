0c7470ac9ff62364f4ed75b4ed696149552a57b0
jjppddDeveloper <jjppdd.userrr@gmail.com>
2023-09-23 07:41:41 +0900

    最低限のテキストボックスとボタンの構築

--- MyApp.cs
+++ MyApp.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 
 class TextBoxSample : Form
 {
+    Button btn;
     TextBox txt;
 
     TextBoxSample()
@@ -16,12 +17,25 @@ class TextBoxSample : Form
             Width = 250,
         });
 
+        Controls.Add(btn = new Button()
+        {
+            Location = new Point(20, 60),
+            Text = "OK"
+        });
+
         txt.TextChanged += Txt_TextChanged;
+
+        btn.Click += Btn_OnClick;
     }
 
     void Txt_TextChanged(object sender, EventArgs e)
     {
         Text = txt.Text;
+    }
+
+    void Btn_OnClick(object sender, EventArgs e)
+    {
+        MessageBox.Show(txt.Text);
     }
 
     [STAThread]
